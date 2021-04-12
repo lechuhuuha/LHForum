32a2a1e175b0bc2bd14d95d6b616d13123c36db3
lechuhuuha <59781669+lechuhuuha@users.noreply.github.com>
2021-04-12 16:50:58 +0700

    new interface

--- app/classes/Lchh/Routes.php
+++ app/classes/Lchh/Routes.php
@@ -5,6 +5,6 @@ namespace Lchh;
 interface Routes
 {
     public function getRoutes(): array;
-    // public function getAuthentication(): \Lchh\Authentication;
+    public function getAuthentication(): \Lchh\Authentication;
     // public function checkPermission($permission): bool;
 }
