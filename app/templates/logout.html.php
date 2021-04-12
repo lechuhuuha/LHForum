d0384d742acd79a30e4bc70387bed00bcb5a9dd8
lechuhuuha <59781669+lechuhuuha@users.noreply.github.com>
2021-04-12 16:53:32 +0700

    logout template

---
+++ app/templates/logout.html.php
@@ -0,0 +1,8 @@
+<div class="col-md-9">
+        <div class="ask-question-input-part032">
+            <h2>You have been logged out</h2>
+
+        </div>
+    </div>
+
+    <!--   end of col-md-9 -->
