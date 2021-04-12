d17d6d73ea3cfc5441ab05fc18f0366fd5a1002e
lechuhuuha <59781669+lechuhuuha@users.noreply.github.com>
2021-04-12 16:54:23 +0700

    register success template

---
+++ app/templates/registersuccess.html.php
@@ -0,0 +1,7 @@
+<div class="col-md-9">
+    <div class="ask-question-input-part032">
+        <h2>Registration Successful</h2>
+        <p>You are now registered on the Internet Ask Forum</p>
+    </div>
+</div>
+<!--   end of col-md-9 -->
