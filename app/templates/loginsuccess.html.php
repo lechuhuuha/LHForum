4b42a664b309b4fe3f45b57fabf6fe28a9428f66
lechuhuuha <59781669+lechuhuuha@users.noreply.github.com>
2021-04-12 16:53:22 +0700

    login success template

---
+++ app/templates/loginsuccess.html.php
@@ -0,0 +1,8 @@
+    <div class="col-md-9">
+        <div class="ask-question-input-part032">
+            <h2>Login Successful</h2>
+            <p>You are now login on the Internet Ask Forum</p>
+        </div>
+    </div>
+
+    <!--   end of col-md-9 -->
