d63f04047f7974c043bf2857f7e551db3e47dc5a
lechuhuuha <59781669+lechuhuuha@users.noreply.github.com>
2021-04-12 16:53:07 +0700

    login error template

---
+++ app/templates/loginerror.html.php
@@ -0,0 +1,8 @@
+<div class="col-md-9">
+    <div class="ask-question-input-part032">
+        <h2>You are not logged in</h2>
+        <p>You must be logged in to view this page. <a href="<?php echo URLROOT ?>login">Click here to log in</a> or <a href="<?php echo URLROOT  ?>user/register">Click here to register an account</a></p>
+    </div>
+</div>
+
+<!--   end of col-md-9 -->
