c4e4c0c03e0cf21273d1ab9a95bc56ce3a0e0949
lechuhuuha <59781669+lechuhuuha@users.noreply.github.com>
2021-04-15 17:05:09 +0700

    login success template

--- app/templates/loginsuccess.html.php
+++ app/templates/loginsuccess.html.php
@@ -2,6 +2,8 @@
         <div class="ask-question-input-part032">
             <h2>Login Successful</h2>
             <p>You are now login on the Internet Ask Forum</p>
+            <p>You will be redirect after 2 seconds</p>
+            <?php header("Refresh:2; url=" . URLROOT  ) ?>
         </div>
     </div>
 
